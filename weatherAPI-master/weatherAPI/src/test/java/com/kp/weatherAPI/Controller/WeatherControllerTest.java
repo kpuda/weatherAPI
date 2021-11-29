57326157d9f2efc6fcb8443dade989d30b5fdb24
krzysztof <kpuda.contact@gmail.com>
2021-11-29 10:40:49 +0100

    Finally added average temperature

--- weatherAPI-master/weatherAPI/src/test/java/com/kp/weatherAPI/Controller/WeatherControllerTest.java
+++ weatherAPI-master/weatherAPI/src/test/java/com/kp/weatherAPI/Controller/WeatherControllerTest.java
@@ -1,62 +1,31 @@
 package com.kp.weatherAPI.Controller;
 
-import com.kp.weatherAPI.EntityDTO.*;
-import com.kp.weatherAPI.Service.Impl.GeometryServiceImpl;
-import com.kp.weatherAPI.Service.Impl.TimeseriesServiceImpl;
-import com.kp.weatherAPI.Service.Impl.WeatherServiceImpl;
-import org.aspectj.lang.annotation.Before;
-import org.hamcrest.Matchers;
+import com.kp.weatherAPI.EntityDTO.WeatherDTO;
+import com.kp.weatherAPI.Service.WeatherService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MockMvcBuilder;
-import org.springframework.test.web.servlet.ResultMatcher;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
-import java.util.List;
-
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.*;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @WebMvcTest(WeatherController.class)
 class WeatherControllerTest {
-
-    @MockBean
-    private WeatherServiceImpl weatherService;
     @Autowired
     private MockMvc mockMvc;
 
     @Autowired
     private WeatherController weatherController;
-
-
-   /* WeatherDTO weatherDTO = new WeatherDTO(
-            new GeometryDTO(19.0, 50.0, 200.0),
-            new PropertiesDTO(
-                    List.of(
-                            new TimeseriesDTO("23-11-2021", List.of(
-                                    new DataDTO("12:00:00", new DetailsDTO(10.0, 5.0, 10.0, 10.0, 12.0, 1.0)))))
-            ));
-    WeatherDTO weatherDTO2 = new WeatherDTO(
-            new GeometryDTO(17.0, 53.0, 200.0),
-            new PropertiesDTO(
-                    List.of(
-                            new TimeseriesDTO("24-11-2021", List.of(
-                                    new DataDTO("12:00:00", new DetailsDTO(10.0, 5.0, 10.0, 10.0, 12.0, 1.0)))))
-            ))*/;
+    @MockBean
+    private WeatherService weatherService;
 
 
     @BeforeEach
     void setUp() throws Exception {
-        List<WeatherDTO> weatherDTOList = (List<WeatherDTO>) weatherService.getWeatherByGeometry(50.0, 19.0);
-        System.out.println("Setup "+weatherDTOList);
     }
 
     @Test
@@ -75,6 +44,8 @@ class WeatherControllerTest {
 
     @Test
     void newWeatherOrder() {
+        WeatherDTO weather= weatherController.newWeatherOrder(50.0,19.0);
+        System.out.println("Yes" +weather);
     }
 
     @Test
@@ -83,12 +54,7 @@ class WeatherControllerTest {
 
     @Test
     void shouldReturnWeatherList() throws Exception {
-        when(weatherService.getWeatherList()).thenReturn(null);
-        System.out.println(weatherService.getWeatherList());
-        this.mockMvc.perform(MockMvcRequestBuilders.get("/weather/list")
-                        .header("X-foo", "Duke"))
-                .andExpect(MockMvcResultMatchers.status().isOk())
-                .andExpect(MockMvcResultMatchers.jsonPath("$.size()", Matchers.is(0)));
+
     }
 
     @Test
